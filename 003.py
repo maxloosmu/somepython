d3b71685c55a0dd35c936b2534e90faf2a5b2181
maxloosmu <maxloo@smu.edu.sg>
2021-10-27 20:05:07 +0800

    3rd commit

---
+++ 003.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+this_dict0 = {}
+print(this_dict0)
+
+this_dict1 = {'a':1, 'b':2, 'c':3}
+print(this_dict1['a'])
+
+this_dict2 = {"a":1, "b":2, "c":3}
+print(this_dict2["a"])
+
+this_dict3 = {"a key":"cat", "b key":"dog", "c key":"bird"}
+print(this_dict3)
+print(this_dict3["a key"])
