b69e3e7430861f852fde9f56ae39a9f1491f4e0d
maxloosmu <maxloo@smu.edu.sg>
2021-11-14 20:18:24 +0800

    7th commit

--- 003.py
+++ 003.py
@@ -5,10 +5,20 @@ print(this_dict0)
 
 this_dict1 = {'a':1, 'b':2, 'c':3}
 print(this_dict1['a'])
+print(this_dict1)
 
 this_dict2 = {"a":1, "b":2, "c":3}
 print(this_dict2["a"])
+print(this_dict2)
 
 this_dict3 = {"a key":"cat", "b key":"dog", "c key":"bird"}
 print(this_dict3)
 print(this_dict3["a key"])
+
+this_list = [[3,4], [1,2]]
+this_dict4 = dict(this_list)
+print(this_list)
+print(this_dict4)
+
+
+
