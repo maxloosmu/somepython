e03ac97781d997d0073d54ee045bade1381f28af
maxloosmu <maxloo@smu.edu.sg>
2021-11-28 20:57:12 +0800

    commit 9.2

--- 008.py
+++ 008.py
@@ -4,22 +4,22 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    def printNameAge(self):
+    def print_name_age(self):
         print(f"{self.name}, {self.age} years old")
 class PersonWithPhone(Person):
     def __init__(self, name, age, phone):
         super().__init__(name, age)
         self.phone = phone
-    def phoneNum(self):
+    def phone_num(self):
         print(f"Phone Number: {self.phone}")
 class PersonWithSecondPhone(PersonWithPhone):
-    def __init__(self, name, age, phone, secondPhone):
+    def __init__(self, name, age, phone, second_phone):
         super().__init__(name, age, phone)
-        self.secondPhone = secondPhone
-    def secondPhoneNum(self):
-        print(f"Second Phone Number: {self.secondPhone}")
+        self.second_phone = second_phone
+    def second_phone_num(self):
+        print(f"Second Phone Number: {self.second_phone}")
 jonathon = PersonWithSecondPhone("Jon", 8, 1111, 2222)
-jonathon.secondPhoneNum()
-jonathon.phoneNum()
-jonathon.printNameAge()
+jonathon.second_phone_num()
+jonathon.phone_num()
+jonathon.print_name_age()
 
